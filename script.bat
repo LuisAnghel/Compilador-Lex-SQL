781042f4596841f72fba3f462e92e69fd60b27be
LuisAnghel <luis@gmail.com>
2022-05-23 10:11:14 -0500

    subiendo los archivos

---
+++ script.bat
@@ -0,0 +1,13 @@
+echo off
+C:\GnuWin32\bin\flex lexmini.l
+echo "FLEX compiló la especificación léxica del archivo lexmini.l (se creó un archivo lex.yy.c)"
+pause
+C:\MinGW\bin\gcc.exe lex.yy.c -o Compilado.exe
+echo "Ha finalizado la ejecución del compilado de C en el archivo lex.yy.c (se creó un archivo Compilado.exe)"
+pause
+Compilado.exe Prueba.txt
+echo "Se ejecutó el analizador léxico sobre el archivo Prueba.txt"
+del lex.yy.c
+del Compilado.exe
+echo "Los archivos generados a lo largo de este proceso fueron eliminados, finalizando..."
+pause
